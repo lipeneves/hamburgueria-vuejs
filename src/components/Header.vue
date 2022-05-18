dba744302f710b442472777388b5a10bba087ce2
Felipe Neves <rj.felipeneves@gmail.com>
2022-05-18 11:59:58 -0300

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/">
+        <img src="" alt="" id="logo">
+      </router-link>
+      
+      <router-link to="/">Home</router-link> |
+      <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+
+export default{
+  name: 'Header'
+}
+
+</script>
